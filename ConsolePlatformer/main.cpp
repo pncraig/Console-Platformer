9a5decaa3f01921502950878f60c08efa2a542ae
pncraig <pncraig03@gmail.com>
2021-01-04 22:10:42 -0500

    Added the basics

--- ConsolePlatformer/main.cpp
+++ ConsolePlatformer/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <windows.h>
 using namespace std;
 
+int nScreenWidth = 120;
+int nScreenHeight = 30;
+
 int main() {
-	cout << "Hello world!" << endl;
+	wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight];
+	HANDLE hconsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	SetConsoleActiveScreenBuffer(hconsole);
+	DWORD dwBytesWritten = 0;
+
+	for (;;) {
+
+		screen[nScreenWidth * nScreenHeight] = L'\0';
+		WriteConsoleOutputCharacter(hconsole, screen, nScreenWidth * nScreenHeight, { 0, 0 }, &dwBytesWritten);
+	}
 
 	return 0;
 }
