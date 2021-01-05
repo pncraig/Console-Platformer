de086f346c63e3702931ccb946cf2e8deccbf365
pncraig <pncraig03@gmail.com>
2021-01-05 14:10:53 -0500

    Added player movement and began collisions

--- ConsolePlatformer/main.cpp
+++ ConsolePlatformer/main.cpp
@@ -11,13 +11,15 @@ using namespace std;
 int nScreenWidth = 120;
 int nScreenHeight = 30;
 
-float gravity = 0.5;
-float friction = 0.1;
+float gravity = 0.5f;
+float friction = 0.1f;
 
 float fPlayerX;
 float fPlayerY;
 float fPlayerVelX = 0.0f;
 float fPlayerVelY = 0.0f;
+float fPlayerHorizontalSpeed = 0.2f;
+float fPlayerJumpSpeed = 0.6f;
 
 struct map {
 	wstring layout;
@@ -38,8 +40,8 @@ int main() {
 	map map1;
 	map1.width = 16;
 	map1.height = 16;
-	map1.playerStartX = 2;
-	map1.playerStartY = 15;
+	map1.playerStartX = 1;
+	map1.playerStartY = 14;
 	map1.layout += L"%%%%%%%%%%%%%%%%";
 	map1.layout += L"%              %";
 	map1.layout += L"%              %";
@@ -54,21 +56,63 @@ int main() {
 	map1.layout += L"%              %";
 	map1.layout += L"%              %";
 	map1.layout += L"%              %";
-	map1.layout += L"%O            @%";
+	map1.layout += L"%             @%";
 	map1.layout += L"%%%%%%%%%%%%%%%%";
 
 	int nCurrentMapIndex = 0;
 	map maps[] = { map1 };
 
-	map& currentMap = maps[nCurrentMapIndex];
+	map currentMap = maps[nCurrentMapIndex];
+	fPlayerX = (float)currentMap.playerStartX;
+	fPlayerY = (float)currentMap.playerStartY;
+
 	for (;;) {
 		this_thread::sleep_for(50ms);
+		// Clear the screen
 		for (int i = 0; i < nScreenWidth * nScreenHeight; i++)
 			screen[i] = L' ';
+		// Reset the map
+		currentMap.layout = maps[nCurrentMapIndex].layout;
 
-		currentMap = maps[nCurrentMapIndex];
+		/*Move the player character -------------------------------------------------------------*/
+
+		if (GetAsyncKeyState((unsigned short)'A') & 0x8000)
+			fPlayerVelX -= fPlayerHorizontalSpeed;
+		if (GetAsyncKeyState((unsigned short)'D') & 0x8000)
+			fPlayerVelX += fPlayerHorizontalSpeed;
+		if (GetAsyncKeyState((unsigned short)'W') & 0x8000)
+			fPlayerVelY -= fPlayerJumpSpeed;
+		
+		fPlayerX += fPlayerVelX;
+		fPlayerY += fPlayerVelY;
+
+		fPlayerVelY += gravity;
+		if (fPlayerVelX != 0.0f) {
+			if (fPlayerVelX > 0.0f)
+				fPlayerVelX -= friction;
+			if (fPlayerVelX < 0.0f)
+				fPlayerVelX += friction;
+		}
+
+		/*Collisions ----------------------------------------------------------------------------*/
+
+		for (int x = 0; x < currentMap.width; x++) {
+			for (int y = 0; y < currentMap.height; y++) {
+				wchar_t currentBlock = currentMap.layout[y * currentMap.width + x];
+
+				if ((int)fPlayerX == x && (int)fPlayerY == y) {
+					switch (currentBlock) {
+						case L'%':
+
+					}
+				}
+			}
+		}
 
 		/*Display the game ----------------------------------------------------------------------*/
+
+		// Update the position of the player character on the map
+		currentMap.layout[(int)fPlayerY * currentMap.width + (int)fPlayerX] = L'O';
 		for (int x = 0; x < currentMap.width; x++) {
 			for (int y = 0; y < currentMap.height; y++) {
 				screen[y * nScreenWidth + x] = currentMap.layout[y * currentMap.width + x];
